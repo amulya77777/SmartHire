51e916b2fa38e2056373d28f74f24e4483c83e1f
amulya77777 <hustleamulya@gmail.com>
2025-11-27 00:18:18 +0530

    fix

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 const corsOptions = {
-  origin: ["http://localhost:5173", "http://localhost:3000", "http://localhost:8000"],
+  origin: [
+    "http://localhost:5173",
+    "http://localhost:3000",
+    "http://localhost:8000",
+  ],
   credentials: true,
 };
 
@@ -26,10 +30,10 @@ app.use(cors(corsOptions));
 const PORT = process.env.PORT || 3000;
 
 // api's
-app.use("/api/v1/user", userRoute);
-app.use("/api/v1/company", companyRoute);
-app.use("/api/v1/job", jobRoute);
-app.use("/api/v1/application", applicationRoute);
+app.use("/api/user", userRoute);
+app.use("/api/company", companyRoute);
+app.use("/api/job", jobRoute);
+app.use("/api/application", applicationRoute);
 
 app.listen(PORT, () => {
   connectDB();
