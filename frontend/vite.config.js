6420929118fbd22dd4adba41fc35bb5ce9c2b405
amulya77777 <hustleamulya@gmail.com>
2025-11-27 00:24:46 +0530

    fix

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,13 +1,13 @@
-import path from "path";
+// import path from "path";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react()],
   base: import.meta.env.VITE_API_KEY || "SmartHire#",
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
+  // resolve: {
+  //   alias: {
+  //     "@": path.resolve(__dirname, "./src"),
+  //   },
+  // },
 });
